4af86cb2d93c84e86632918d40481f3c38a3d604
KALU56 <kalkidanbehailu55@gmail.com>
2026-08-26 13:40:07 +0300

    feat: implement home and new note screens with reusable UI components

---
+++ notability_clone/lib/core/widgets/app_icon_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../theme/app_spacing.dart';
+
+class AppIconButton extends StatelessWidget {
+  final IconData icon;
+  final VoidCallback? onPressed;
+  final String? tooltip;
+  final Color? color;
+  final double size;
+
+  const AppIconButton({
+    super.key,
+    required this.icon,
+    this.onPressed,
+    this.tooltip,
+    this.color,
+    this.size = 24.0,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Widget button = IconButton(
+      icon: Icon(icon, size: size, color: color),
+      onPressed: onPressed,
+      padding: AppSpacing.paddingSm,
+      constraints: const BoxConstraints(minWidth: 44, minHeight: 44),
+    );
+
+    if (tooltip != null) {
+      return Tooltip(message: tooltip!, child: button);
+    }
+    return button;
+  }
+}
