df037350bc94e97fefd1c9c0da4aa75d7f7e9b8e
KALU56 <kalkidanbehailu55@gmail.com>
2026-08-31 17:04:13 +0300

    feat: implement paper customization system and note editor screen with template support

--- notability_clone/lib/core/routes/app_routes.dart
+++ notability_clone/lib/core/routes/app_routes.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
 import '../../features/library/presentation/home_screen.dart';
 import '../../features/library/presentation/new_note_screen.dart';
+import '../../features/notes/presentation/note_editor_screen.dart';
 
 abstract class AppRoutes {
   static const String home = '/';
   static const String editor = '/editor';
+  static const String noteEditor = '/note_editor';
 
   static Map<String, WidgetBuilder> get routes {
     return {
       home: (context) => const HomeScreen(),
       editor: (context) => const NewNoteScreen(),
+      noteEditor: (context) {
+        final args = ModalRoute.of(context)?.settings.arguments;
+        if (args is Map<String, dynamic>) {
+          return NoteEditorScreen(
+            title: args['title'] as String? ?? 'Untitled Note',
+          );
+        }
+        return const NoteEditorScreen();
+      },
     };
   }
 }
