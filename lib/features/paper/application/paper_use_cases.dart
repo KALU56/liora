e20928c8e002e4a0b3161754d33f41c473f6954b
KALU56 <kalkidanbehailu55@gmail.com>
2026-09-24 15:10:03 +0300

    feat: add PaperUseCases class for managing paper template updates

---
+++ lib/features/paper/application/paper_use_cases.dart
@@ -0,0 +1,13 @@
+import '../domain/models/paper_template.dart';
+
+/// Application operations for changing the paper configuration of a page.
+class PaperUseCases {
+  const PaperUseCases();
+
+  PaperTemplate updateTemplate(
+    PaperTemplate current,
+    PaperTemplate updated,
+  ) {
+    return updated;
+  }
+}
