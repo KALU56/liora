0986f7e5acc3baf70f02eb28b1b7c9026b7e8436
KALU56 <kalkidanbehailu55@gmail.com>
2026-09-24 15:07:36 +0300

    refactor: replace Color with ArgbColor in ToolConfig for consistent color representation

--- lib/features/canvas/domain/models/writing_tool.dart
+++ lib/features/canvas/domain/models/writing_tool.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'argb_color.dart';
 
 enum WritingToolType { pen, pencil, highlighter, eraser }
 
@@ -12,14 +12,14 @@ enum EraserSize {
 
 class ToolConfig {
   final WritingToolType toolType;
-  final Color color;
+  final ArgbColor color;
   final double strokeWidth;
   final double opacity;
   final EraserSize eraserSize;
 
   const ToolConfig({
     this.toolType = WritingToolType.pen,
-    this.color = Colors.black,
+    this.color = const ArgbColor(0xFF000000),
     this.strokeWidth = 3.0,
     this.opacity = 1.0,
     this.eraserSize = EraserSize.small,
@@ -27,7 +27,7 @@ class ToolConfig {
 
   ToolConfig copyWith({
     WritingToolType? toolType,
-    Color? color,
+    ArgbColor? color,
     double? strokeWidth,
     double? opacity,
     EraserSize? eraserSize,
@@ -41,55 +41,24 @@ class ToolConfig {
     );
   }
 
-  Map<String, dynamic> toMap() {
-    return {
-      'toolType': toolType.name,
-      'color': color.toARGB32(),
-      'strokeWidth': strokeWidth,
-      'opacity': opacity,
-      'eraserSize': eraserSize.name,
-    };
-  }
-
-  Map<String, dynamic> toJson() => toMap();
-
-  factory ToolConfig.fromMap(Map<String, dynamic> map) {
-    return ToolConfig(
-      toolType: WritingToolType.values.firstWhere(
-        (type) => type.name == map['toolType'],
-        orElse: () => WritingToolType.pen,
-      ),
-      color: Color((map['color'] as num?)?.toInt() ?? Colors.black.toARGB32()),
-      strokeWidth: (map['strokeWidth'] as num?)?.toDouble() ?? 3.0,
-      opacity: (map['opacity'] as num?)?.toDouble() ?? 1.0,
-      eraserSize: EraserSize.values.firstWhere(
-        (size) => size.name == map['eraserSize'],
-        orElse: () => EraserSize.small,
-      ),
-    );
-  }
-
-  factory ToolConfig.fromJson(Map<String, dynamic> json) =>
-      ToolConfig.fromMap(json);
-
   /// Preset configurations for quick tool selection
   static const ToolConfig defaultPen = ToolConfig(
     toolType: WritingToolType.pen,
-    color: Colors.black,
+    color: ArgbColor(0xFF000000),
     strokeWidth: 3.0,
     opacity: 1.0,
   );
 
   static const ToolConfig defaultPencil = ToolConfig(
     toolType: WritingToolType.pencil,
-    color: Color(0xFF4A4A4A),
+    color: ArgbColor(0xFF4A4A4A),
     strokeWidth: 2.0,
     opacity: 0.8,
   );
 
   static const ToolConfig defaultHighlighter = ToolConfig(
     toolType: WritingToolType.highlighter,
-    color: Color(0xFFFFEB3B), // Bright Yellow
+    color: ArgbColor(0xFFFFEB3B),
     strokeWidth: 16.0,
     opacity: 0.4,
   );
