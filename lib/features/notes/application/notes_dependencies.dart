c0ea7b89d177339fd84ff0473b939c45e16f893b
KALU56 <kalkidanbehailu55@gmail.com>
2026-09-24 15:09:00 +0300

    chore: remove notes_dependencies.dart as it is no longer needed

--- lib/features/notes/application/notes_dependencies.dart
+++
@@ -1,6 +0,0 @@
-import '../data/repositories/note_repository.dart';
-import 'notes_use_cases.dart';
-
-final NotesUseCases appNotesUseCases = NotesUseCases(appNoteRepository);
-
-Future<void> initializeNotes() => appNoteRepository.initialize();
